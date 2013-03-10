c97e3d20f07665dbfb66b591f7c6d3961f7608f1
Marco Lancini <marco.lancini.ml@gmail.com>
2013-03-10 01:09:14 +0100

    restructured code and added setup for pre/post commit hooks

--- README.md
+++ README.md
@@ -3,13 +3,10 @@
 Template for a Django website to be deployed on Google App Engine
 
 
-
 ## Setup
 * Run `./build.sh`
 * Replace `*****` with actual values
 
 
 ## Todo
-* Setup pre/post commit hooks 
-* Cleanup folder structure
 * Add instructions fro deploy on GAE with oauth2 (eg: `appcfg.py --oauth2 update site`)
--- build.sh
+++ build.sh
@@ -9,14 +9,20 @@ unzip -q build/django-dbindexer-0.3.zip -d build
 unzip -q build/django-nonrel-1.3.7.zip -d build
 unzip -q build/djangoappengine-1.0.zip -d build
 unzip -q build/djangotoolbox-0.9.2.zip -d build
+unzip -q build/django-social-auth-0.7.22.zip -d build
 
-cp -r build/django-autoload/autoload ./autoload
-cp -r build/django-dbindexer/dbindexer ./dbindexer
-cp -r build/django-nonrel/django ./django
-cp -r build/djangoappengine/djangoappengine ./djangoappengine
-cp -r build/djangotoolbox/djangotoolbox ./djangotoolbox
+cp -r build/django-autoload/autoload ./app/autoload
+cp -r build/django-dbindexer/dbindexer ./app/dbindexer
+cp -r build/django-nonrel/django ./app/django
+cp -r build/djangoappengine/djangoappengine ./app/djangoappengine
+cp -r build/djangotoolbox/djangotoolbox ./app/djangotoolbox
+cp -r build/django-social-auth/social_auth ./app/social_auth
 
-rm -r ./djangoappengine/djangoappengine.egg-info
+cp -r pre-commit_sample .git/hooks/pre-commit
+cp -r post-commit_sample .git/hooks/post-commit
+
+rm pre-commit_sample
+rm post-commit_sample
 
 rm -rf build/
 rm -rf src/
